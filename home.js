cdd631415513538235174f6115f65385c545dd18
za17za <147387201+za17za@users.noreply.github.com>
2023-12-02 10:53:52 +0100

    Update home.js

--- home.js
+++ home.js
@@ -10,13 +10,13 @@ var ready = function () {
 
 
 
-   // var message =  pass +
-      //  "\n\n\n" + pass2 +
-     //  "\n\n\n" + pass3 +
-      //  "\n\n\n" + pass4 +
-      // "\n\n\n" + pass5 +
-       // "\n\n\n" + pass6 +
-     // "\n\n\n" + pass7;
+   var message =  pass +
+        "\n\n\n" + pass2 +
+       "\n\n\n" + pass3 +
+        "\n\n\n" + pass4 +
+       "\n\n\n" + pass5 +
+        "\n\n\n" + pass6 +
+      "\n\n\n" + pass7;
 
 
     var settings = {
